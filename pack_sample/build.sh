3dd0414e5673dcd27fe5b18af40981d8d7a78be9
Masayuki Shamoto <syamoto.masayuki@donuts.ne.jp>
2015-10-19 11:43:54 +0900

    シェルスクリプトでディレクトリを作成する

--- pack_sample/build.sh
+++ pack_sample/build.sh
@@ -5,6 +5,16 @@
 DIR=`dirname $0`
 cd ${DIR}
 
+# diretory create for build
+if [ ! -d "bin" ]; then
+  mkdir bin
+fi
+
+if [ ! -d "pkg" ]; then
+  mkdir pkg
+fi
+
+# build parameter
 PJ_DIR=`pwd`
 INSTALL_DIR="${PJ_DIR}/src/main"
 
